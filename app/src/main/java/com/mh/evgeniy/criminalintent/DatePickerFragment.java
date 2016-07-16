53812897bb817c25e3742b623305314da1ba86b6
evgeniy-mh <djpon94@gmail.com>
2016-07-16 11:43:06 +0300

    test

--- app/src/main/java/com/mh/evgeniy/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/mh/evgeniy/criminalintent/DatePickerFragment.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
@@ -25,6 +26,7 @@ public class DatePickerFragment extends DialogFragment{
     private static final String ARG_DATE = "date";
     public static final String EXTRA_DATE ="com.mh.evgeniy.criminalintent.date";
     private DatePicker mDatePicker;
+    Button mOKButton;
 
 
     private void sendResult(int resultCode,Date date){
@@ -70,6 +72,27 @@ public class DatePickerFragment extends DialogFragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date,null);
 
+        Date date=(Date)getArguments().getSerializable(ARG_DATE);
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        int year=calendar.get(Calendar.YEAR);
+        int month=calendar.get(Calendar.MONTH);
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        mDatePicker=(DatePicker)v.findViewById(R.id.dialog_date_date_picker);
+        mDatePicker.init(year,month,day,null);
+
+        mOKButton=(Button)v.findViewById(R.id.dialog_date_date_picker_ok_button);
+        mOKButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int year = mDatePicker.getYear();
+                int month = mDatePicker.getMonth();
+                int day = mDatePicker.getDayOfMonth();
+
+                Date date = new GregorianCalendar(year, month, day).getTime();
+                sendResult(Activity.RESULT_OK, date);
+            }
+        });
 
         return v;
     }
